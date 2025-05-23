ef4c3ebf6532762270a5a5a6c0dc642048e245d2
gc-accounts <jatin@odinschool.com>
2025-05-23 14:51:55 +0530

    CORS Resolved

--- url-shortener-backend/server.js
+++ url-shortener-backend/server.js
@@ -13,7 +13,7 @@ mongoose.connect('mongodb+srv://technology:mLtQuWzm1UrCAyoZ@cluster0.2akwggi.mon
 
 // CORS configuration for frontend and localhost
 app.use(cors({
-  origin: ['https://odinly-tool.vercel.app'],
+  origin: ['https://odinly-tool-t3m2.vercel.app/'],
   methods: ['GET', 'POST'],
   credentials: true
 }));
